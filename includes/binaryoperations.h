00120e97b6782a7a8c89aabbb1038362ea6fa824
JeanCassiano <115122121+JeanCassiano@users.noreply.github.com>
2023-09-27 12:28:58 -0300

    Atualizações com .h

---
+++ includes/binaryoperations.h
@@ -0,0 +1,49 @@
+#ifndef BINARYOPERATIONS_H
+#define BINARYOPERATIONS_H
+
+#include <stdio.h>
+
+
+// Cabeçalho com 13 bytes
+typedef struct {
+    char status;  // 1 para arquivo consistente, 0 para incosistente
+    int proxRRN;
+    int nroTecnologias;
+    int nroParesTecnologias;
+} Cabecalho;
+
+typedef struct {
+    int tamanho;
+    char* string;
+} StringVariavel;
+
+// Registro com 76 bytes
+typedef struct {
+    char removido; // 1 para removido e 0 para não removido
+
+    // seguintes campos presentes no arquivo csv fornecido pela professora
+    int grupo;
+    int popularidade;
+    int peso;
+    StringVariavel tecnologiaOrigem;
+    StringVariavel tecnologiaDestino;
+
+    
+} Registro;
+
+// Função para abrir um arquivo binário para leitura
+FILE *abrirArquivoLeitura(const char *nomeArquivo);
+
+// Função para abrir um arquivo binário para escrita
+FILE *abrirArquivoEscrita(const char *nomeArquivo);
+
+// Função para fechar um arquivo binário
+void fecharArquivo(FILE *arquivo);
+
+// Função para ler um registro do arquivo binário
+int lerRegistro(FILE *arquivo, Registro *registro);
+
+// Função para escrever um registro no arquivo binário
+int escreverRegistro(FILE *arquivo, const Registro *registro);
+
+#endif /* BINARYOPERATIONS_H */
